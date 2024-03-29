61146eb15f6386b79b84e243313855cde5a5e934
Ismael López <ismael97lopez@gmail.com>
2024-03-29 15:01:47 -0700

    fix error

--- src/app/api/reservation/route.js
+++ src/app/api/reservation/route.js
@@ -5,9 +5,29 @@ export async function POST(req) {
   const body = await req.json();
   try {
     const { userId, classId, position } = body;
-    const res = await prisma.reservation.create({
-      data: { user_id: userId, class_id: classId, position },
+    const [canReserv] = await prisma.membership.findMany({
+      where: { user_id: userId, is_active: true },
+      select: {
+        id: true,
+        days_to_access: true,
+      },
     });
+    if (canReserv.days_to_access <= 0) {
+      throw new Error('User with no classes available');
+    }
+    const res = await Promise.all([
+      prisma.reservation.create({
+        data: { user_id: userId, class_id: classId, position },
+      }),
+      prisma.membership.update({
+        where: { id: canReserv.id },
+        data: {
+          days_to_access: {
+            increment: -1,
+          },
+        },
+      }),
+    ]);
     return NextResponse.json(res, { status: 200 });
   } catch (error) {
     console.error('Error fetching user:', error);
